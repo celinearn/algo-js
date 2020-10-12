83a68a812b8f492d8cd8d6dc0385e0d5f66cd100
System Administrator <root@MacBook-Pro-de-Celine.local>
2020-10-12 16:01:14 +0200

    2.5

---
+++ js/2.5.js
@@ -0,0 +1,13 @@
+const readlineSync = require('readline-sync');
+let number = readlineSync.question('What is your favorite number?')
+
+while (number != 42) {
+	number = readlineSync.question('Are you sure?');
+}
+
+
+
+
+//Make a program that asks the favorite number of the user. 
+//If that number is anything other than 42 display "Are you sure?" and ask again. 
+//(This program should never end as long as the user didn't chose 42).

f87d84a84175d1b55c8a1c665c890e6e5e2d7923
System Administrator <root@MacBook-Pro-de-Celine.local>
2020-10-12 10:27:08 +0200

    1.6

---
+++ js/1.6.js
@@ -0,0 +1,10 @@
+const readlineSync = require("readline-sync");
+let integer = readlineSync.question('Give me a number please')
+let integer2 = readlineSync.question('Give me an other one please')
+
+console.log(integer/integer2);
+
+
+
+//Ask two integers to the user. Display the rest of the integer division of the two numbers.
+

4e707a81bdb10ded198728b4dd89bc0ecfe9f037
System Administrator <root@MacBook-Pro-de-Celine.local>
2020-10-12 15:45:23 +0200

    2.4

---
+++ js/2.4.js
@@ -0,0 +1,12 @@
+let i = 1;
+
+while (i <= 100) {
+  console.log(i*3);
+  i += 1;
+  console.log(i/2);
+  i += 1;
+}
+
+
+//Count from 1 to 100. For every even number display the result of their division by 2. 
+//For every odd number display the result of their multiplication by 3.

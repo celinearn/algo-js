1af6528180f7e9949948a5e57e7503d617f20b23
System Administrator <root@MacBook-Pro-de-Celine.local>
2020-10-12 10:44:27 +0200

    1.7

---
+++ js/1.7.js
@@ -0,0 +1,24 @@
+const readlineSync = require("readline-sync");
+let shoe = readlineSync.question('What are your shoe size please?')
+let birth = readlineSync.question('What are your birth year please?')
+
+
+console.log(shoe * 2);
+let double = (shoe * 2);
+console.log(double + 5);
+let multiply = (double + 5);
+console.log(multiply * 50);
+let result = (multiply * 50);
+console.log(birth - result);
+let resultat = (birth - result);
+console.log(resultat + 1766);
+
+
+
+
+//Multiply the shoe size by 2
+//Add 5 to the result
+//Multiply that by 50
+//Subtract the birth year
+//Add 1766
+//Test with your own birth year and your shoe size.

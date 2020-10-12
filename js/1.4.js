dfe981a802d8c824e0303df728516f72a827bfc2
System Administrator <root@MacBook-Pro-de-Celine.local>
2020-10-12 10:02:39 +0200

    1.4

---
+++ js/1.4.js
@@ -0,0 +1,11 @@
+const readlineSync = require("readline-sync");
+let userfirstName = readlineSync.question('Can you give me your first name please?');
+let userName = readlineSync.question('Can you give me your name please?');
+let usercity = readlineSync.question('Can you give me your city please?');
+
+
+console.log("Your name is" + " " + userfirstName + " " + userName + " " + "and you live in" + " " + usercity + ".");
+
+
+//Make this same thing as Exercise 2 but this time ask the user for the name, the first name and the city.
+

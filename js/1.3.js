c3ada0c1aa5bb134b65f6965acdb54e42f527284
System Administrator <root@MacBook-Pro-de-Celine.local>
2020-10-12 09:53:46 +0200

    1.3

---
+++ js/1.3.js
@@ -0,0 +1,9 @@
+const readlineSync = require("readline-sync");
+let userName = readlineSync.question('Can you give me your name please?');
+console.log("Hello " + userName);
+
+
+
+
+//Ask the user to enter its first name and display a message saying "Hello first name".
+

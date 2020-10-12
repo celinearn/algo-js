7a36bf74b340c61c2a8fe7f96515795683336656
System Administrator <root@MacBook-Pro-de-Celine.local>
2020-10-12 09:46:52 +0200

    1.1 and 1.2

---
+++ js/1.2.js
@@ -0,0 +1,12 @@
+let name = "Lambert";
+let firstName = "Gerard";
+let city = "Paris";
+
+console.log("Your name is" + " " + firstName + " " + name + " " + "and you live in" + " " + city + ".")
+
+
+
+
+
+//Define three variables: name, firstName and city. Display them like this: "Your name is Gerard Lambert and you live in Paris.".
+

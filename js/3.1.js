7597c98ee02bd6bf79eb453c4e7d5cc1c21d8f13
System Administrator <root@MacBook-Pro-de-Celine.local>
2020-10-13 10:38:06 +0200

    3.1 and 3.2

---
+++ js/3.1.js
@@ -0,0 +1,13 @@
+let arr = [1, 2, 3, 4, 5];
+let arrr = [100, 101, 102];
+
+console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+console.log(arrr[0] + arrr[1] + arrr[2]);
+
+
+//Write a program that will add all the elements of an array.
+
+//Test it with the following arrays:
+
+//[1, 2, 3, 4, 5] => 15
+//[100, 101, 102] => 303
---
+++ js/3.2.js
@@ -0,0 +1,13 @@
+let arr = [1, 2, 3, 4, 5]
+let arr2 = [100, 101, 102]
+
+console.log(arr[2]);
+console.log(arr2[1]);
+
+
+//Write a program that will calculate the average value of a given array.
+
+//Test it with the following arrays:
+
+//[1, 2, 3, 4, 5] => 3
+//[100, 101, 102] => 101
